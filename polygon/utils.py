a70f7ccf3dec5cf2a351d6789da3717758ac0166
OlegGayvoronsky <o.gajvoronskij03@gmail.com>
2025-03-28 21:35:50 +0300

    Добавил логику сегментов

---
+++ polygon/utils.py
@@ -0,0 +1,50 @@
+import numpy as np
+import cv2
+from scipy.interpolate import interp1d
+
+def create_segment(segment):
+    target_fps = 30
+    fps = len(segment)
+    segment = np.array(segment)
+    indices = []
+
+
+    if fps > target_fps:
+        indices = np.linspace(0, fps - 1, target_fps, dtype=int)
+        resampled_segment = segment[indices]
+    elif target_fps > fps > 1:
+        resampled_segment = []
+        add_count = target_fps - fps
+        for i, joints in enumerate(segment):
+            if i > 0 and add_count > 0:
+                _joints = np.ndarray([17, 2], dtype=int)
+                add_count -= 1
+                for j in range(17):
+                    _x, _y = joints[j - 1]
+                    x, y = joints[j]
+                    _joints[j][0] = (max(_x, x) + min(_x, x)) / 2
+                    _joints[j][1] = (max(_y, y) + min(_y, y)) / 2
+                indices.append(i)
+                resampled_segment.append(joints)
+            indices.append(i)
+            resampled_segment.append(joints)
+        resampled_segment = np.array(resampled_segment)
+    else:
+        resampled_segment = segment
+
+    return resampled_segment, indices
+
+
+def show(frames, orig_imgs,  ids, video_writer):
+
+    images = np.array(orig_imgs)[ids, :, :]
+
+    for f, img in zip(frames, images):
+        h, w, _ = img.shape
+        for joint in f:
+            x, y = joint
+            cv2.circle(img, (int(x * w), int(y * h)), 5, (0, 0, 255), -1)
+        img = np.array(img, dtype=np.uint8)  # Преобразуем в формат uint8 (если нужно)
+        video_writer.write(img)
+
+    cv2.destroyAllWindows()
--- polygon/yoloy11n-pose.py
+++ polygon/yoloy11n-pose.py
@@ -1,29 +1,64 @@
+from utils import create_segment, show
 import cv2
+import numpy as np
+import time
 import torch
 from ultralytics import YOLO
 
 model = YOLO("yolo11n-pose.pt")
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 model.to(device)
+start = time.time()
 
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+frame_size = (640, 480)
+
+# Создаем объект для записи видео
+video_writer = cv2.VideoWriter("./videos/output.mp4", fourcc, 30, frame_size)
 cap = cv2.VideoCapture(0)
 
+cadr = 0
+frames = []
+batch = 0
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
 
-    with torch.no_grad():
-        results = model.predict(frame)
+    # with torch.no_grad():
+    #      result = model.predict(frame)
+    #
+    # frame = result[0].plot()
+    # cv2.imshow("Pose Detection", frame)
+    #
+    # torch.cuda.empty_cache()
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
 
-    annotated_frame = results[0].plot()
+    cadr += 1
+    frames.append(frame)
+    end = time.time()
+    if end - start >= 1:
+        batch += 1
+        if batch == 1:
+            frames = []
+            start = time.time()
+            cadr = 0
+            continue
 
-    cv2.imshow("Pose Detection", annotated_frame)
+        with torch.no_grad():
+            results = model.predict(frames)
+        segment = [result.keypoints.xyn[0].cpu().numpy() for result in results]
 
-    del annotated_frame
-    torch.cuda.empty_cache()
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        orig_imgs = [result.orig_img for result in results]
+        segment, ids = create_segment(segment)
+        show(segment, orig_imgs, ids, video_writer)
+
+        frames = []
+        start = time.time()
+        print(start - end)
+        cadr = 0
+        if batch == 11: break
 
 del results
 torch.cuda.empty_cache()
