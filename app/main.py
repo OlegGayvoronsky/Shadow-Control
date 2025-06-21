2ada4fc43787071ce282d5e3c04b5f6a40015bc4
OlegGayvoronsky <o.gajvoronskij03@gmail.com>
2025-06-21 23:46:56 +0300

    Добавил проверку на микрофон и камеру

--- app/main.py
+++ app/main.py
@@ -2,17 +2,18 @@ import json
 import sys
 import os
 from pathlib import Path
-
+import sounddevice as sd
+import cv2
 from PySide6.QtWidgets import QApplication, QDialog
 from ui.steam_path_dialog import SteamPathDialog
-from ui.camera_select_dialog import CameraSelectDialog
 from logic.steam_path_search import get_steam_path, save_steam_path
+
+cnfg_pth = str(Path(__file__).resolve().parent / Path("config"))
 
 def is_valid_steam_path(path):
     return path and os.path.isdir(path) and os.path.exists(os.path.join(path, "steam.exe"))
 
 def save_camera_micro_index(index, micro_index):
-    cnfg_pth = str(Path(__file__).resolve().parent / Path("config"))
     with open(cnfg_pth + "/camera_micro_index.json", "w") as f:
         json.dump({"camera_index": index, "micro_index": micro_index}, f)
 
@@ -30,9 +31,35 @@ if __name__ == "__main__":
 
         steam_path = get_steam_path()
 
-    cam_dialog = CameraSelectDialog(on_camera_selected=save_camera_micro_index)
-    if cam_dialog.exec() != QDialog.Accepted:
-        sys.exit()
+    flag = False
+    if not os.path.exists(cnfg_pth + "/camera_micro_index.json"):
+        flag = True
+    else:
+        with open(cnfg_pth + "/camera_micro_index.json", "r") as f:
+            data = json.load(f)
+            index = data.get("camera_index")
+            micro_index = data.get("micro_index")
+        cap = cv2.VideoCapture(index, cv2.CAP_DSHOW)
+        if not cap.isOpened():
+            flag = True
+        if cap:
+            cap.release()
+
+        devices = sd.query_devices()
+        for i, dev in enumerate(devices):
+            if dev['max_input_channels'] > 0 and i == micro_index:
+                try:
+                    with sd.InputStream(device=i, channels=1, samplerate=16000):
+                        break
+                except Exception:
+                    flag = True
+                    break
+
+    if flag:
+        from ui.camera_select_dialog import CameraSelectDialog
+        cam_dialog = CameraSelectDialog(on_camera_selected=save_camera_micro_index)
+        if cam_dialog.exec() != QDialog.Accepted:
+            sys.exit()
 
     from ui.main_menu_window import MainMenu
     main_menu = MainMenu()
